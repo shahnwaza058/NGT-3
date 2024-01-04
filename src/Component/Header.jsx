cab650821b2cd6a549025785f2345b6aa0980fdc
shahnwazansari <shahnwaz.ansari@nagarro.com>
2024-01-04 19:35:13 +0530

    firs

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -9,22 +9,6 @@ const Header = () => {
       <div className="w-100 bg-dark text-white p-2">
         <p className="text-center h1 m-0 fs-5 fw-bolder">NGT-PROJECT</p>
       </div>
-      <div className="icons">
-        <Button
-          variant="danger"
-          size="sm"
-          className=" m-2 text-capitalize text-white"
-        >
-          <IoIosSave color="white" size={25} /> save
-        </Button>
-        <Button
-          variant="secondary"
-          size="sm"
-          className="m-2 text-capitalize text-white"
-        >
-          <FaCloudDownloadAlt color="white" size={25} /> download
-        </Button>
-      </div>
     </div>
   );
 };
--- src/Component/SpreadSheetTable.jsx
+++ src/Component/SpreadSheetTable.jsx
@@ -10,88 +10,37 @@ import {
   CheckboxRendererMixin,
   Parser,
 } from "@gridsheet/react-core";
-// Hello
+import { Button } from "react-bootstrap";
+import { IoIosSave } from "react-icons/io";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+const XLSX = require("xlsx");
+
 const SpreadSheetTable = () => {
   const initialData = [
     ["Country", "Gender", "Age Group", "2019", "2020", "2021", "2022", "2023"],
-    ["Country1", "Male", "Age 20-40", 2, 3, 5, 2, 6],
-    ["Country1", "Male", "Age 40-60", 2, 3, 5, 2, 6],
-    ["Country1", "Male", "Age 60-90", 2, 3, 5, 2, 6],
-    ["", "Male Total"],
-    ["Country1", "Female", "Age 20-40", 2, 3, 5, 2, 6],
-    ["Country1", "Female", "Age 40-60", 3, 5, 2, 1, 2],
-    ["Country1", "Female", "Age 60-90", 1, 2, 3, 2, 5],
-    [
-      "",
-      "Female Total",
-      "",
-      "=SUM(D6:D8)",
-      "=SUM(E6:E8)",
-      "=SUM(F6:F8)",
-      "=SUM(G6:G8)",
-      "=SUM(H6:H8)",
-    ],
-    [
-      "",
-      "Country1 Total",
-      "",
-      "=SUM(D5,D9)",
-      "=SUM(E5,E9)",
-      "=SUM(F5,F9)",
-      "=SUM(G5,G9)",
-      "=SUM(H5,H9)",
-    ],
-    ["Country2", "Male", "Age 40-60", 3, 5, 2, 1, 2],
-    ["Country2", "Male", "Age 20-40", 2, 3, 5, 2, 6],
-    ["Country2", "Male", "Age 60-90", 1, 2, 3, 2, 5],
-    [
-      "",
-      "Male Total",
-      "",
-      "=SUM(D11:D13)",
-      "=SUM(E11:E13)",
-      "=SUM(F11:F13)",
-      "=SUM(G11:G13)",
-      "=SUM(H11:H13)",
-    ],
-    ["Country2", "Female", "Age 20-40", 2, 3, 5, 2, 6],
-    ["Country2", "Female", "Age 40-60", 3, 5, 2, 1, 2],
-    ["Country2", "Female", "Age 60-90", 1, 2, 3, 2, 5],
-    [
-      "",
-      "Female Total",
-      "",
-      "=SUM(D15:D17)",
-      "=SUM(E15:E17)",
-      "=SUM(F15:F17)",
-      "=SUM(G15:G17)",
-      "=SUM(H15:H17)",
-    ],
-    [
-      "",
-      "Country2 Total",
-      "",
-      "=SUM(D14,D18)",
-      "=SUM(E14,E18)",
-      "=SUM(F14,F18)",
-      "=SUM(G14,G18)",
-      "=SUM(H14,H18)",
-    ],
-    [
-      "",
-      "Grand Total",
-      "",
-      "=SUM(D10,D19)",
-      "=SUM(E10,E19)",
-      "=SUM(F10,F19)",
-      "=SUM(G10,G19)",
-      "=SUM(H10,H19)",
-    ],
+    ["Country1", "Male", "Age 20-40", 0, 0, 0, 0, 0],
+    ["Country1", "Male", "Age 40-60", 0, 0, 0, 0, 0],
+    ["Country1", "Male", "Age 60-90", 0, 0, 0, 0, 0],
+    ["", "Total"],
+    ["Country1", "Female", "Age 20-40", 0, 0, 0, 0, 0],
+    ["Country1", "Female", "Age 40-60", 0, 0, 0, 0, 0],
+    ["Country1", "Female", "Age 60-90", 0, 0, 0, 0, 0],
+    ["", "Total", ""],
+    ["", "Sub Total", ""],
+    ["Country2", "Male", "Age 40-60", 0, 0, 0, 0, 0],
+    ["Country2", "Male", "Age 20-40", 0, 0, 0, 0, 0],
+    ["Country2", "Male", "Age 60-90", 0, 0, 0, 0, 0],
+    ["", "Total", ""],
+    ["Country2", "Female", "Age 20-40", 0, 0, 0, 0, 0],
+    ["Country2", "Female", "Age 40-60", 0, 0, 0, 0, 0],
+    ["Country2", "Female", "Age 60-90", 0, 0, 0, 0, 0],
+    ["", "Total", ""],
+    ["", "Sub Total", ""],
+    ["", "Grand Total", ""],
   ];
   const tableRef = createTableRef();
   const [data, setData] = useState(initialData);
-
-  // Function to calculate sums based on country and gender
+  const [tsv, setTsv] = useState("");
   const calculateSums = (tabledata) => {
     const sums = [];
 
@@ -100,9 +49,8 @@ const SpreadSheetTable = () => {
     tabledata?.forEach((row) => {
       const country = row[0];
       const gender = row[1];
-      const value = row.slice(3); // Considering all columns after the third as year values
+      const value = row.slice(3);
 
-      // Ensure the row is not a total row and has valid data
       if (country && gender) {
         const existingEntry = sums.find(
           (entry) => entry.country === country && entry.gender === gender
@@ -145,39 +93,36 @@ const SpreadSheetTable = () => {
   const calculateYearlyCountrySums = (tabledata) => {
     const sums = [];
 
-    const years = {}; // To keep track of unique years
-    console.log("first");
-    if (tabledata)
-      for (let i = 1; i < tabledata?.length; i++) {
-        const country = tabledata[i][0];
-        const value = tabledata[i].slice(3); // Considering all columns after the third as year values
-        console.log("length:", country.length);
-        // Ensure the row is not a total row and has valid data
-        if (country) {
-          const existingEntry = sums.find((entry) => entry.country === country);
+    const years = {};
+    // console.log("first");
 
-          if (!existingEntry) {
-            const newEntry = {
-              country,
-              total: Array(value.length).fill(0), // Initialize totals for each year as an array
-            };
-            sums.push(newEntry);
-          }
+    for (let i = 1; i < tabledata?.length; i++) {
+      const country = tabledata[i][0];
+      const value = tabledata[i].slice(3);
 
-          value.forEach((val, index) => {
-            if (typeof val == "number") {
-              sums.find((entry) => entry.country === country).total[index] +=
-                val;
+      if (country) {
+        const existingEntry = sums.find((entry) => entry.country === country);
 
-              if (!years[index]) {
-                years[index] = true;
-              }
-            }
-          });
+        if (!existingEntry) {
+          const newEntry = {
+            country,
+            total: Array(value.length).fill(0),
+          };
+          sums.push(newEntry);
         }
-      }
 
-    // Add the unique years to the sums objects
+        value.forEach((val, index) => {
+          if (typeof val == "number") {
+            sums.find((entry) => entry.country === country).total[index] += val;
+
+            if (!years[index]) {
+              years[index] = true;
+            }
+          }
+        });
+      }
+    }
+
     sums.forEach((entry) => {
       entry.years = Object.keys(years).map((yearIndex) => ({
         year: parseInt(yearIndex) + 2019, // Assuming starting from 2019 based on the index
@@ -187,35 +132,50 @@ const SpreadSheetTable = () => {
 
     return sums;
   };
+  const calculateTotalYearWise = (data) => {
+    const yearWiseTotal = {};
+
+    data.forEach((countryData) => {
+      countryData.years.forEach((yearData) => {
+        const { year, total } = yearData;
+        if (!yearWiseTotal[year]) {
+          yearWiseTotal[year] = 0;
+        }
+        yearWiseTotal[year] += total;
+      });
+    });
+
+    return Object.keys(yearWiseTotal).map((year) => ({
+      year: parseInt(year),
+      total: yearWiseTotal[year],
+    }));
+  };
 
   const calculateSumsAndAppend = (matrix) => {
-    const sums = calculateSums(matrix); // Assuming you have a function to calculate sums
-    // const yearWiseSum = calculateYearlyCountrySums(matrix);
+    const sums = calculateSums(matrix);
+    const yearWiseSum = calculateYearlyCountrySums(matrix);
+    const grandTotal = calculateTotalYearWise(yearWiseSum);
     // console.log("sumsss", yearWiseSum);
-    const updatedData = [...matrix]; // Copy the existing data
+    // console.log("yearsdata", grandTotal);
+    const updatedData = [...matrix];
     var subTotalIdx = [];
     for (let i = 1; i < sums.length; i++) {
       const { country, gender, years } = sums[i];
-
-      // Find the index of the last male and female in the specified country
       let insertIndex = -1;
       let tempIdx = -1;
       updatedData.forEach((row, index) => {
         if (row[0] === country && row[1] === gender) {
           insertIndex = index;
         }
-        if (row[0] === country) 
-        {
+        if (row[0] === country) {
           tempIdx = index;
-
         }
       });
       const existingEntry = subTotalIdx.find((entry) => entry === tempIdx);
       if (!existingEntry) subTotalIdx.push(tempIdx);
       if (tableRef.current) {
         const { dispatch, table } = tableRef.current;
-
-        //update year wise sum dynamically
+        //Appending category wise total sum
         for (let i = 0; i < years.length; i++) {
           dispatch(
             table.write({
@@ -224,24 +184,63 @@ const SpreadSheetTable = () => {
             })
           );
         }
+        //Appending country wise total sum
+        for (let i = 0; i < subTotalIdx.length; i++) {
+          let totals = yearWiseSum[i]?.total;
+          totals.forEach((total, idx) => {
+            dispatch(
+              table.write({
+                point: { x: idx + 4, y: subTotalIdx[i] + 3 },
+                value: total.toString(),
+              })
+            );
+          });
+        }
+        //Appending Grand Total
+        for (let i = 0; i < grandTotal.length; i++) {
+          dispatch(
+            table.write({
+              point: { x: i + 4, y: updatedData.length },
+              value: grandTotal[i]?.total.toString(),
+            })
+          );
+        }
       }
     }
-    // subTotalIdx.forEach(idx=>(
-
-    // ))
-
-    console.log(subTotalIdx);
   };
 
   const handleTable = (table) => {
-    // console.log(table);
     const matrix = table.getMatrixFlatten({});
-    // console.log(matrix);
     setData(matrix);
     calculateSumsAndAppend(matrix);
   };
+
+  const handleDownloadData = (table) => {
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet(table);
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    XLSX.writeFile(workbook, "output.xlsx");
+  };
   return (
     <div className="grid-container bg-light">
+      <div className="icons">
+        <Button
+          disabled
+          variant="danger"
+          size="sm"
+          className=" m-2 text-capitalize text-white"
+        >
+          <IoIosSave color="white" size={25} /> save
+        </Button>
+        <Button
+          onClick={() => handleDownloadData(data)}
+          variant="secondary"
+          size="sm"
+          className="m-2 text-capitalize text-white"
+        >
+          <FaCloudDownloadAlt color="white" size={25} /> download
+        </Button>
+      </div>
       <GridSheet
         tableRef={tableRef}
         // style={{ minWidth: "80%" }}
@@ -269,14 +268,14 @@ const SpreadSheetTable = () => {
             A: { width: 100 },
             B: { width: 120 },
             C: { width: 100 },
-            // D: { width: 50 },
-            // E: { width: 50 },
-            // F: { width: 50 },
-            // G: { width: 50 },
-            // H: { width: 50 },
           },
         })}
         options={{
+          onSave: (table) => {
+            const matrix = table.getMatrixFlatten({});
+            setTsv(matrix.map((cols) => cols.join("\t")).join("\n"));
+            handleDownloadData(matrix);
+          },
           onChange: (table) => handleTable(table),
         }}
       />{" "}
